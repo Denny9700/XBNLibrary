64c79f83306de73ac7c301998e06ce7439aba458
Denny9700 <denny9700@gmail.com>
2019-03-09 17:13:04 +0100

    Added XBN Library

---
+++ XBNLibrary/XBNLibrary/structure/XBN_Header.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace XBNLibrary.structure
+{
+  public class XBN_Header
+  {
+    public byte[] Head { get; internal set; }
+
+    public List<string> Tags { get; set; }
+    public List<string> Properties { get; set; }
+
+    public XBN_Header()
+    {
+      this.Head = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xCD, 0xCC, 0xCC, 0x3D };
+
+      this.Tags = new List<string>();
+      this.Properties = new List<string>();
+    }
+
+    public static XBN_Header Deserialize(BinaryReader reader)
+    {
+      if(reader == null)
+        throw new Exception("Reader is null");
+
+      var head = reader.ReadBytes(8);
+      var tagCount = reader.ReadInt16();
+
+      List<string> tags = new List<string>();
+      for(int i = 0; i < tagCount; i++)
+        tags.Add(reader.ReadXBNStr());
+
+      var propertyCount = reader.ReadInt16();
+
+      List<string> properties = new List<string>();
+      for(int i = 0; i < propertyCount; i++)
+        properties.Add(reader.ReadXBNStr());
+
+      return new XBN_Header
+      {
+        Head = head,
+        Tags = tags,
+        Properties = properties
+      };
+    }
+
+    public void Serialize(BinaryWriter writer)
+    {
+      if(writer == null)
+        throw new Exception("Writer is null");
+
+      writer.Write(this.Head);
+      writer.Write((short)this.Tags.Count);
+
+      for(int i = 0; i < this.Tags.Count; i++)
+        writer.WriteXBNStr(this.Tags[i]);
+
+      writer.Write((short)this.Properties.Count);
+
+      for(int i = 0; i < this.Properties.Count; i++)
+        writer.WriteXBNStr(this.Properties[i]);
+    }
+  }
+}
---
+++ XBNLibrary/XBNLibrary/structure/XBN_Property.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace XBNLibrary.structure
+{
+  public class XBN_Property
+  {
+    private XBN_Header Header { get; set; }
+
+    public string Key { get; set; }
+    public string Value { get; set; }
+
+    public XBN_Property(XBN_Header header)
+    {
+      this.Header = header;
+    }
+
+    public XBN_Property(XBN_Header header, string key, string value)
+      : this(header)
+    {
+      this.Key = key;
+      this.Value = value;
+    }
+
+    public static XBN_Property Deserialize(BinaryReader reader, XBN_Header header)
+    {
+      if (reader == null)
+        throw new Exception("Reader is null");
+
+      if (header == null)
+        throw new Exception("Header is null");
+
+      var propertyKey = header.Properties[reader.ReadInt16()];
+      var propertyValue = reader.ReadXBNStr();
+
+      return new XBN_Property(header, propertyKey, propertyValue);
+    }
+
+    public void Serialize(BinaryWriter writer)
+    {
+      if (writer == null)
+        throw new Exception("Writer is null");
+
+      writer.Write((short)this.Header.Properties.IndexOf(this.Key));
+      writer.WriteXBNStr(this.Value);
+    }
+  }
+}
